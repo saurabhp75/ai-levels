869867662f426da6991fe5ba909825bae41d90e4
Saurabh Prakash <saurabhp75@gmail.com>
2025-08-24 06:09:23 +0530

    Show tool calls on front end.

--- courses/01-deepsearch-in-typescript/00-apps/01-day-1-app/src/app/chat.tsx
+++ courses/01-deepsearch-in-typescript/00-apps/01-day-1-app/src/app/chat.tsx
@@ -53,7 +53,7 @@ export const ChatPage = ({ userName, isAuthenticated }: ChatProps) => {
             return (
               <ChatMessage
                 key={index}
-                text={message.content}
+                parts={message.parts ?? []}
                 role={message.role}
                 userName={userName}
               />
